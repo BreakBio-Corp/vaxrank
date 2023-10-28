3c7e4631f2080975b20f7bb09f2294105e0b853c
Emon46 <hremon331046@gmail.com>
2023-10-29 01:53:20 +0600

    #wip-test remove cut_off totally to test mhc ligands flexibility
    Signed-off-by: Emon46 <hremon331046@gmail.com>

--- vaxrank/epitope_prediction.py
+++ vaxrank/epitope_prediction.py
@@ -82,8 +82,8 @@ class EpitopePrediction(Serializable):
         TODO: Use a large dataset to find MHC binding range predicted to #
         correlate with immunogenicity
         """
-        if self.percentile_rank >= el_score_cut_off:
-            return 0.0
+        # if self.percentile_rank >= el_score_cut_off:
+        #     return 0.0
         return self.el_score
 
     def slice_source_sequence(self, start_offset, end_offset):
